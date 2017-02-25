4cfa8d9e771dea14727ba28368cb45dc5dfef442
Maxwell Wogsland <Maxwell Wogsland>
2017-02-25 12:56:59 -0600

    resolved conflict

--- splash/views.py
+++ splash/views.py
@@ -13,8 +13,4 @@ beaconlist =  beaconlist + "</table>"
 hello = "<link rel=\"shortcut icon\" type=\"image/png\" href=\"/favicon.ico\"/>Hello, world.<br>"
 
 def index(request):
-<<<<<<< HEAD
-    return HttpResponse("<link rel=\"shortcut icon\" type=\"image/png\" href=\"/favicon.ico\"/>https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg")
-=======
     return HttpResponse(beaconlist)
->>>>>>> ce4a3b07c4488f95bf592e675cb58813c5fe8e8f
