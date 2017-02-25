607869ff2cc6d124ebcbf06611a68761689c8b8f
Maxwell Wogsland <Maxwell Wogsland>
2017-02-25 12:33:06 -0600

    p

--- Beacon/settings.py
+++ Beacon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5t=+p5#)&8)1yev9i#kixr6cyqzou$#fib^t9w&iqjz)8mwyjt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -122,4 +122,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/favicon.ico/'
--- splash/views.py
+++ splash/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<link rel=\"shortcut icon\" type=\"image/png\" href=\"/favicon.ico\"/>Hello, world.")
+    return HttpResponse("<link rel=\"shortcut icon\" type=\"image/png\" href=\"/favicon.ico\"/>https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg")
