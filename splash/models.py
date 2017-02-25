ce4a3b07c4488f95bf592e675cb58813c5fe8e8f
Bradley Wogsland <bradley@wogsland.org>
2017-02-25 13:03:24 -0500

    models in view

--- splash/models.py
+++ splash/models.py
@@ -5,16 +5,19 @@ from django.db import models
 # Create your models here.
 class Beacons(models.Model):
     station_id = models.IntegerField()
-    beacon_id = models.IntegerField()
+    beacon_id = models.IntegerField(primary_key=True)
     beacon_uuid = models.CharField(max_length=200)
     beacon_string = models.CharField(max_length=200)
 
+    class Meta:
+        db_table = 'beacons'
+
 class Beacon_Reading(models.Model):
-    beacon_reading_id = models.IntegerField()
+    beacon_reading_id = models.IntegerField(primary_key=True)
     beacon_id = models.IntegerField()
     user_id = models.IntegerField()
     distance = models.CharField(max_length=200)
 
 class Stations(models.Model):
-    station_id = models.IntegerField()
+    station_id = models.IntegerField(primary_key=True)
     station_name = models.CharField(max_length=200)
--- splash/views.py
+++ splash/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from splash.models import Beacons
+
+beacons = Beacons.objects.all()
+
+beaconlist = "<table>"
+for beacon in beacons:
+    beaconlist = beaconlist + "<tr><td>" + str(beacon.beacon_id) + "</td>"
+    beaconlist = beaconlist + "<td>" + beacon.beacon_string + "</td></tr>"
+beaconlist =  beaconlist + "</table>"
+
+hello = "<link rel=\"shortcut icon\" type=\"image/png\" href=\"/favicon.ico\"/>Hello, world.<br>"
 
 def index(request):
-    return HttpResponse("<link rel=\"shortcut icon\" type=\"image/png\" href=\"/favicon.ico\"/>Hello, world.")
+    return HttpResponse(beaconlist)
