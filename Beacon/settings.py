e7b1f8e1c9d3bf6d99db22d058e80b3efe64cc46
Bradley Wogsland <bradley@wogsland.org>
2017-02-25 14:50:05 -0500

    that worked

--- Beacon/settings.py
+++ Beacon/settings.py
@@ -79,12 +79,12 @@ DATABASES = {
     'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#        'ENGINE': 'django.db.backends.mysql',
-#        'OPTIONS': {
-#            'read_default_file': 'Beacon/my.cnf',
-#        },
-#    },
-#    'production': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'Beacon/my.cnf',
+        },
+    },
+    'production': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ['MYSQL_DATABASE'],
         'USER': os.environ['MYSQL_USER'],
