939cbc6350b835432c9d9d39a905d6168c402859
Bradley Wogsland <bradley@wogsland.org>
2017-02-25 10:31:54 -0500

    pip freeze - the ugly way to generate requirements...

--- Beacon/settings.py
+++ Beacon/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'Beacon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#        'ENGINE': 'django.db.backends.mysql',
-#        'OPTIONS': {
-#            'read_default_file': 'Beacon/my.cnf',
-#        },
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'my.cnf',
+        },
     }
 }
 
