6fdb3c1f43674102544342896c477606529ee445
Bradley Wogsland <bradley@wogsland.org>
2017-02-25 15:52:41 -0500

    graph of beacon usage

--- splash/views.py
+++ splash/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from splash.models import Beacons
+from splash.models import Beacons, Beacon_Reading
+from django.db.models import Count
 
 beacons = Beacons.objects.all()
 
@@ -9,6 +10,12 @@ for beacon in beacons:
     beaconlist = beaconlist + "<tr><td>" + str(beacon.beacon_id) + "</td>"
     beaconlist = beaconlist + "<td>" + beacon.beacon_string + "</td></tr>"
 beaconlist =  beaconlist + "</table>"
+
+querycounts = Beacon_Reading.objects.values("beacon_id").annotate(Count("beacon_id")).order_by()
+beaconcounts = ""
+for querycount in querycounts:
+    print(querycount)
+    beaconcounts = beaconcounts + "," + str(querycount['beacon_id__count'])
 
 hello = "<link rel=\"shortcut icon\" type=\"image/png\" href=\"/favicon.ico\"/>Hello, world.<br>"
 
@@ -38,6 +45,7 @@ template = """
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 
 </head>
@@ -108,19 +116,19 @@ template = """
 
         <!-- Second Featurette -->
         <div class="featurette" id="services">
-            <img class="featurette-image img-circle img-responsive pull-left" src="">
             <h2 class="featurette-heading">Realtime Data
                 <span class="text-muted">Number of People by each Beacon</span>
             </h2>
-            <p class="lead">To the left is a live pie chart of the number of devices near each beacon(every beacon is a different color).</p>
+            <p class="lead">Below is a live pie chart of the number of devices near each beacon(every beacon is a different color).</p>
+            <canvas id="myChart" width="400" height="400" class="pull-left"></canvas>
         </div>
 
         <hr class="featurette-divider">
 
         <!-- Third Featurette -->
         <div class="featurette" id="contact">
-            <img class="featurette-image img-circle img-responsive pull-right" src="http://placehold.it/500x500">
-            <h2 class="featurette-heading">The
+                <img class="featurette-image img-circle img-responsive pull-right" src="">
+                <h2 class="featurette-heading">The
                 <span class="text-muted">Squad</span>
             </h2>
             <p class="lead">Our team! We had a great time. Thanks to MARTA and everyone else for this opportunity.</p>
@@ -132,7 +140,7 @@ template = """
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; MARTA Beacons 2017</p>
                 </div>
             </div>
         </footer>
@@ -145,6 +153,46 @@ template = """
 
     <!-- Bootstrap Core JavaScript -->
     <script src="/static/js/bootstrap.min.js"></script>
+
+    <script>
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Beacon 1", "Beacon 2", "Beacon 3", "Beacon 4", "Beacon 5", "Beacon 6"],
+            datasets: [{
+                label: 'people',
+                data: [""" + beaconcounts +"""],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    </script>
 
 </body>
 
