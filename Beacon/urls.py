b688b72f020fb336c3fdad327cae720b24d79139
Bradley Wogsland <bradley@wogsland.org>
2017-02-25 00:07:15 -0500

    the favicon that does not work

--- Beacon/urls.py
+++ Beacon/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
+    url(r'^favicon\.ico$', favicon_view),
     url(r'^splash/', include('splash.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- splash/views.py
+++ splash/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return HttpResponse("<link rel=\"shortcut icon\" type=\"image/png\" href=\"/favicon.ico\"/>Hello, world.")
